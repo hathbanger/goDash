54714217ca7f6d64eef03e501be149439e6a91fb
Hathaway <ahatha200@comcast.com>
2017-12-12 20:25:12 -0700

    change proj name

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "Build Docker"
-docker build -t gcr.io/serene/dash-api:$TRAVIS_COMMIT .
-docker tag gcr.io/serene/dash-api:$TRAVIS_COMMIT gcr.io/serene/dash-api:latest
+docker build -t gcr.io/serene-188901/dash-api:$TRAVIS_COMMIT .
+docker tag gcr.io/serene-188901/dash-api:$TRAVIS_COMMIT gcr.io/serene-188901/dash-api:latest
 echo "Authenticate Google Cloud Engine"
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
@@ -12,10 +12,10 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 echo "Push Docker Image"
-gcloud docker -- push gcr.io/serene/dash-api
-yes | gcloud beta container images add-tag gcr.io/serene/dash-api:$TRAVIS_COMMIT gcr.io/serene/dash-api:latest
+gcloud docker -- push gcr.io/serene-188901/dash-api
+yes | gcloud beta container images add-tag gcr.io/serene-188901/dash-api:$TRAVIS_COMMIT gcr.io/serene-188901/dash-api:latest
 echo "Configure Kubernetes"
 kubectl config view
 kubectl config current-context
 echo "Deploy"
-kubectl set image deployment/dash-api dash-api=gcr.io/serene/dash-api:$TRAVIS_COMMIT
+kubectl set image deployment/dash-api dash-api=gcr.io/serene-188901/dash-api:$TRAVIS_COMMIT
