545bc24484a8f69d192166dec89ec0a380814782
Hathaway <ahatha200@comcast.com>
2017-12-12 20:17:09 -0700

    gcr

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "Build Docker"
-docker build -t hathbanger/dash-api:$TRAVIS_COMMIT .
-docker tag hathbanger/dash-api:$TRAVIS_COMMIT hathbanger/dash-api:latest
+docker build -t gcr.io/hathbanger/dash-api:$TRAVIS_COMMIT .
+docker tag gcr.io/hathbanger/dash-api:$TRAVIS_COMMIT gcr.io/hathbanger/dash-api:latest
 echo "Authenticate Google Cloud Engine"
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
@@ -12,11 +12,10 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 echo "Push Docker Image"
-docker tag dash-api hathbanger/dash-api:$TRAVIS_COMMIT
-docker push hathbanger/dash-api
-# yes | gcloud beta container images add-tag hathbanger/dash-api:$TRAVIS_COMMIT hathbanger/dash-api:latest
+gcloud docker -- push gcr.io/hathbanger/dash-api
+yes | gcloud beta container images add-tag gcr.io/hathbanger/dash-api:$TRAVIS_COMMIT gcr.io/hathbanger/dash-api:latest
 echo "Configure Kubernetes"
 kubectl config view
 kubectl config current-context
 echo "Deploy"
-kubectl set image deployment/dash-api dash-api=hathbanger/dash-api:$TRAVIS_COMMIT
+kubectl set image deployment/dash-api dash-api=gcr.io/hathbanger/dash-api:$TRAVIS_COMMIT
